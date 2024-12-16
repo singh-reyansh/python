16bf097ba1283c2ea89c134629545dc12c260355
Arvinderpal10 <arvinderpal10@gmail.com>
2024-12-16 08:12:30 +0530

    Printing different trypes of data

---
+++ Day3/004.printing.py
@@ -0,0 +1,45 @@
+x = 20 
+y = 300.40
+is_valid = True
+name = "John"
+
+print(x)
+print(y)
+print(is_valid)
+print(name)
+
+# My name is John and my age is 20 and my marks are 300.40 and Am I married? True
+
+print("My name is", name, "and my age is", x, "and my marks are", y, "and Am I married?", is_valid)
+
+
+print("My name is :", name)
+
+
+# print{"Hello"}
+#  Print("Hello")
+# printing("Hello")     
+# Traceback (most recent call last):
+#   File "c:\Users\Admin\Desktop\python\Day3\004.printing.py", line 19, in <module>
+#     printing("Hello")        
+#     ^^^^^^^^
+# NameError: name 'printing' is not defined        
+
+
+# print() -- print() is a function that prints the value of the variable to the console
+
+# it is built in function -- provided by python
+
+# type() -- type() is a function that returns the type of the variable -- it is a built in function
+
+# int() -- int() is a function that converts the value to integer -- it is a built in function
+
+# float() -- float() is a function that converts the value to float -- it is a built in function
+
+# str() -- str() is a function that converts the value to string -- it is a built in function
+
+# bool() -- bool() is a function that converts the value to boolean -- it is a built in function
+
+
+
+
