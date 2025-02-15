2e5ebd6d3e9a0bb7e6b958ae81529c0ab73cb88a
singh-reyansh <singh.reyansh0930@gmail.com>
2025-02-15 18:01:25 -0500

    joke machine

---
+++ practice/001.print/joke machine.py
@@ -0,0 +1,24 @@
+#greet the user
+print("Welcome to the joke machine, enter an answer and get a joke!")
+print()
+#Store an animal input as a variable
+animal = input("name an animal : ")
+#print their answer and change their mind
+print("You chose" , animal, "thats a terrible idea , those are not funny at all. Let's use a cow instead!")
+#ask them a question
+joke_1 = input("What do you call a cow with no legs?")
+#print the joke
+print("Nope! Not", joke_1, "You call it GROUND BEEF!")
+print()
+
+body_part = input("Name a body part: ")
+print("You chose" , body_part, "Thats gross. Let's tell a joke about eyes instead.")
+joke_2 = input("What did one eye say to the other?")
+print("sure,", joke_2, "is funny. I guess. But they actually said: Eye think we make a great pair!")
+print()
+
+subject = input("Name a subject in school: ")
+print("You chose" , subject, ", not my favorite.Let's tell a joke about math.")
+joke_3 = input("Why do plants hate math? ")
+print("Because", joke_3, "!? NO WAY! The answer is it always gives them square roots!")
+print()
