2ac04a9974dca304538792b617b2d0473831ce7c
Arvinderpal10 <arvinderpal10@gmail.com>
2025-03-03 09:00:58 +0530

    S1-March - Coding Questions

---
+++ Tests/Practice/Day/test1.py
@@ -0,0 +1,12 @@
+'''
+1. Write a program to reverse a string.
+
+2. Write a program to find the sum of first 10 natural numbers.
+
+3.  Write a Python program to check if a Number is Even or Odd.
+
+4. Write a Python program to Count Words in a Sentence.
+
+5. Write a Python program to convert Celsius to Fahrenheit.
+
+'''
