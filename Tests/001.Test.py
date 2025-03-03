0259136eabbb8b814604810ab9d8f2d1c86f8510
Arvinderpal10 <arvinderpal10@gmail.com>
2025-03-03 07:31:14 +0530

    Test 1 - Python

---
+++ Tests/001.Test.py
@@ -0,0 +1,186 @@
+
+''''
+1. Who developed Python Programming Language?
+  a) Wick van Rossum                           
+  b) Rasmus Lerdorf 
+  c) Guido van Rossum
+  d) Niene Stom       
+
+  2. Which type of Programming does Python support?
+  a) object-oriented programming
+  b) structured programming
+  c) functional programming
+  d) all of the mentioned
+
+   3. Is Python case sensitive when dealing with identifiers?
+  a) no
+  b) yes
+  c) machine dependent
+  d) none of the mentioned
+
+  4. Which of the following is the correct extension of the Python file?
+  a) .python                                                            
+  b) .pl    
+  c) .py
+  d) .p 
+
+  5. Is Python code compiled or interpreted?
+  a) Python code is both compiled and interpreted
+  b) Python code is neither compiled nor interpreted
+  c) Python code is only compiled
+  d) Python code is only interpreted
+
+  6. All keywords in Python are in _________
+  a) Capitalized                            
+  b) lower case 
+  c) UPPER CASE 
+  d) None of trhe above
+                       
+  7. What will be the value of the following Python expression?
+     4 + 3 % 5
+  a) 7
+  b) 2
+  c) 4 
+  d) 1
+
+  8. Which of the following is used to define a block of code in Python language?
+  a) Indentation
+  b) Key
+  c) Brackets
+  d) All of the mentioned
+
+  9. Which keyword is used for function in Python language?
+  a) Function                                              
+  b) Def     
+  c) Fun
+  d) Define
+           
+  10. Which of the following character is used to give single-line comments in Python?
+  a) //
+  b) #
+  c) !
+  d) /*
+
+  11. Which of the following functions can help us to find the version of python that we are currently
+  working on?
+  a) sys.version(1)
+  b) sys.version(0)
+  c) sys.version() 
+  d) sys.version 
+
+  12. Which of the following is true for variable names in Python?
+  a) underscore and ampersand are the only two special characters allowed
+  b) unlimited length
+  c) all private members must have leading and trailing underscores
+  d) none of the mentioned      
+
+  13. Which of the following is the truncation division operator in Python?
+  a) |
+  b) //
+  c) /
+  d) %
+
+  14. Which of the following functions is a built-in function in python?
+  a) factorial()                                                        
+  b) print()    
+  c) seed()
+  d) sqrt()
+
+  15. What will be the output of the following Python function?
+     min(max(False,-3,-4), 2,7)
+  a) -4
+  b) -3
+  c) 2
+  d) False
+        
+  16. Which of the following is not a core data type in Python programming?
+  a) Tuples
+  b) Lists
+  c) Class
+  d) Dictionary
+
+  17. What will be the output of the following Python expression if x=56.236?
+     print("%.2f"%x)
+  a) 56.236
+  b) 56.23 
+  c) 56.0000
+  d) 56.24 
+
+  18. What will be the output of the following Python function?
+     len("Python Test 1")
+  a) Error
+  b) 13
+  c) 12
+  d)14          
+
+  19. What will be the output of the following Python code?
+    x = 'Python Test'
+    print(x.upper())
+  a) Python TEST
+  b) PYTHON TEST
+  c) error 
+  d) PYTHON Test
+
+  20. What arithmetic operators cannot be used with strings in Python?
+  a) *                                                                
+  b) –
+  c) +    
+  d) All of the above
+                        
+  21. What will be the output of the following Python code?
+     print("python. test".capitalize())
+  a) Apython. TEST
+  b) Python. def
+  c) Python. test
+  d) PYTHON. TEST
+
+  22. What will be the output of the following Python program?
+  print("Hello" + 123)
+  a) error
+  b) Hello123
+  c) Hello 123
+  d) none of the mentioned
+
+  23. What will be the output of the following Python code?
+     print(type('True'))
+  a) error
+  b) string
+  c) boolean
+  d) Numeric
+
+  22. What will be the output of the following Python program?
+    print(bool(-1)+100)
+  a) 99
+  b) 100
+  c) error
+  d) 101
+
+    23. What will be the output of the following Python program?
+    print(bool(-1)+1)
+  a) 2
+  b) 0
+  c) error
+  d) 1
+
+    24. What will be the output of the following Python program?
+    print(bool(100)+100)
+  a) 100
+  b) 101
+  c) error
+  d) 200
+
+  25. Which of the following is the use of id() function in python?
+  a) Every object in Python doesn’t have a unique id               
+  b) In Python Id function returns the identity of the object
+  c) None of the mentioned                                    
+  d) All of the mentioned 
+
+  26. What is use of type() function in Python?
+  a) It returns the type of the obeject              
+  b) It is a function to take input
+  c) it is use to calculate the size of the variable                                   
+  d) All of the mentioned 
+  
+    
+
+'''
